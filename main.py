44bbe1c811717d2b305e0df81a6e8d805c99f303
weezstevenson <notthecrochunter@gmail.com>
2022-08-05 09:13:56 -0400

    Initial commit pt 2.

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Weezy 2')
+    print_hi('Weezy 3')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
